f75fd6553ca5c1576a8a811abc6096673e1c59d7
Krislv4nk <kristinalev4enko89@gmail.com>
2024-06-12 18:11:13 +0200

    config css and json

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx'; 
 import './index.css';
+import "@fontsource/roboto";
+import "@fontsource/roboto/400.css"; 
+import "@fontsource/roboto/400-italic.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
