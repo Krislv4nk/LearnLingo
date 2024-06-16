218bcde77e10ebb6797da736986557e6924d506c
Krislv4nk <kristinalev4enko89@gmail.com>
2024-06-16 13:15:03 +0200

    mobile menu done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,22 @@
+
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import css from './Header.module.css';
 import sprite from '../../assets/sprite.svg';
+import { MenuMob } from '../Header/MenuMob/MenuMob';
+import { StyledEngineProvider } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
 
 export const Header = () => {
 
+  const [openMenuMob, setOpenMenuMob] = useState(false);
+
+    const handleOpenClick = () => {
+        setOpenMenuMob(true);
+    }
+    const handleCloseClick = () => {
+        setOpenMenuMob(false);
+    };
   return (
     <header className={css.header}>
       <div className={css.logo}>
@@ -14,7 +27,7 @@ export const Header = () => {
             <p className={css.logoText}>LearnLingo</p>
         </Link>
     </div>
-      <nav>
+      <nav className={css.nav}>
 <ul className={css.navList}>
   <li><Link className={css.link} to="/home">
          Home
@@ -26,10 +39,22 @@ export const Header = () => {
       </nav>
       <ul className={css.authBox}>
         <li><button className={css.logInButton} type='button'><svg className={css.iconLogIn} width={20} height={20}>
-              <use href={`${sprite}#icon-log-in`}></use>
+              <use xlinkHref={`${sprite}#icon-log-in`}></use>
              </svg>Log In</button></li>
         <li><button className={css.registrButton} type='button'>Registration</button></li>
       </ul>
+      <button className={css.mobileMenuButton} onClick={handleOpenClick} type='button' title='Menu'>
+        <svg width={20} height={20}>
+          <use xlinkHref={`${sprite}#icon-menu`}></use>
+        </svg>
+      </button>
+    
+      <StyledEngineProvider injectFirst>
+                    <Dialog open={openMenuMob} onClose={handleCloseClick}
+                        PaperComponent={() =>
+                            <MenuMob onClose={handleCloseClick}  
+                            />}></Dialog>
+                </StyledEngineProvider>
     </header>
   );
 };
---
+++ src/components/Header/MenuMob/MenuMob.jsx
@@ -0,0 +1,38 @@
+
+import { Link } from 'react-router-dom';
+import css from './MenuMob.module.css';
+import sprite from '../../../assets/sprite.svg';
+
+export const MenuMob = ({onClose}) => {
+
+  return (
+  
+      <div className={css.menuDrop}>
+        <div className={css.wrapper}>
+      
+        <button className={css.iconClose} onClick={onClose}>
+          <svg width={20} height={20}>
+            <use xlinkHref={`${sprite}#icon-cross`}></use>
+          </svg>
+        </button>
+        <nav className={css.nav}>
+<ul className={css.navList}>
+  <li><Link className={css.link} to="/home">
+         Home
+        </Link></li>
+  <li> <Link className={css.link} to="/teachers">
+          Teachers
+        </Link></li>
+</ul>
+      </nav>
+      <ul className={css.authBox}>
+        <li><button className={css.logInButton} type='button'><svg className={css.iconLogIn} width={20} height={20}>
+              <use xlinkHref={`${sprite}#icon-log-in`}></use>
+             </svg>Log In</button></li>
+        <li><button className={css.registrButton} type='button'>Registration</button></li>
+      </ul>
+      </div>
+      </div>
+    )
+ 
+};
