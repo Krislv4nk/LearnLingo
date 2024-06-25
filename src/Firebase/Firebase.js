5eeb13c57d3e285a9ac1fbd159c50c987fd0320e
Krislv4nk <kristinalev4enko89@gmail.com>
2024-06-25 15:41:38 +0200

    fixed constants

--- src/Firebase/Firebase.js
+++ src/Firebase/Firebase.js
@@ -1,16 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import * as constants from './constants';
 
 
-const apiKey = import.meta.env.VITE_FIREBASE_API_KEY;
-const authDomain = import.meta.env.VITE_FIREBASE_AUTH_DOMAIN;
-const databaseURL = import.meta.env.VITE_FIREBASE_URL;
-const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
-const storageBucket = import.meta.env.VITE_FIREBASE_STORAGE_BUCKET;
-const messagingSenderId = import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID;
-const appId = import.meta.env.VITE_FIREBASE_APP_ID;
-const measurementId = import.meta.env.VITE_FIREBASE_MEASUREMENT_ID;
+const apiKey = constants.VITE_FIREBASE_API_KEY;
+const authDomain = constants.VITE_FIREBASE_AUTH_DOMAIN;
+const databaseURL = constants.VITE_FIREBASE_URL;
+const projectId = constants.VITE_FIREBASE_PROJECT_ID;
+const storageBucket = constants.VITE_FIREBASE_STORAGE_BUCKET;
+const messagingSenderId = constants.VITE_FIREBASE_MESSAGING_SENDER_ID;
+const appId = constants.VITE_FIREBASE_APP_ID;
+const measurementId = constants.VITE_FIREBASE_MEASUREMENT_ID;
 
 const firebaseConfig = {
   apiKey,
---
+++ src/Firebase/constants.js
@@ -0,0 +1,10 @@
+
+
+export const VITE_FIREBASE_API_KEY="AIzaSyAss2VtgNTE5NQ6l4Z10P8aNmf_Je_P_hk";
+export const VITE_FIREBASE_AUTH_DOMAIN="learnlingo-krislv4nk.firebaseapp.com";
+export const VITE_FIREBASE_URL="https://learnlingo-krislv4nk-default-rtdb.firebaseio.com";
+export const VITE_FIREBASE_PROJECT_ID="learnlingo-krislv4nk";
+export const VITE_FIREBASE_STORAGE_BUCKET="learnlingo-krislv4nk.appspot.com";
+export const VITE_FIREBASE_MESSAGING_SENDER_ID="1052296576541";
+export const VITE_FIREBASE_APP_ID="1:1052296576541:web:34d6e07ffee8ff527290f4";
+export const VITE_FIREBASE_MEASUREMENT_ID="G-W1M1EWXV6T";
