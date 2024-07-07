1b9e7b43c6eb2e2deac81b236f60ca13c8c4bd5f
Krislv4nk <kristinalev4enko89@gmail.com>
2024-07-07 02:33:08 +0200

    fixed fetchTeachers

--- src/components/TeachersList/TeacherCard/TeacherCard.jsx
+++ src/components/TeachersList/TeacherCard/TeacherCard.jsx
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import sprite from '../../../assets/sprite.svg';
 
 export const TeacherCard = ({ teacher }) => {
@@ -17,9 +18,15 @@ export const TeacherCard = ({ teacher }) => {
     experience
   } = teacher;
 
+    const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+    };
+    
   return (
     <li key={teacher.index}>
-      <img src={avatar_url[0]} alt="avatar" />
+      <img src={avatar_url} alt="avatar" />
       <div>
         <div>
           <p>Languages</p>
@@ -32,22 +39,24 @@ export const TeacherCard = ({ teacher }) => {
             </li>
             <span></span>
             <li>
-              <p>Lessons done:</p>{lessons_done}</li>
+              <p>Lessons done:</p>{lessons_done}
+            </li>
             <span></span>
             <li>
               <svg width={20} height={20}>
-                <use xlinkHref={`${sprite}#icon-book`}></use>
+                <use xlinkHref={`${sprite}#icon-star`}></use>
               </svg>
-              <p>Rating</p>{rating}</li>
+              <p>Rating</p>{rating}
+            </li>
             <span></span>
             <li>
-              <p>Price / 1 hour</p>{price_per_hour}</li>
+              <p>Price / 1 hour</p>{price_per_hour}
+            </li>
           </ul>
           <button>
             <svg width={20} height={20}>
               <use xlinkHref={`${sprite}#icon-like`}></use>
             </svg>
-            Lessons online
           </button>
         </div>
         <h2>{name} {surname}</h2>
@@ -66,19 +75,31 @@ export const TeacherCard = ({ teacher }) => {
           <li>
             <p><span>Conditions:</span> {conditions}</p>
           </li>
-          <li>
-            <p>{experience}</p>
-          </li>
         </ul>
-        <ul>
-          {reviews.map((review, index) => (
-            <li key={index}>
-              <p>{review.comment}</p>
-              <p>Reviewer: {review.reviewer_name}</p>
-              <p>Rating: {review.reviewer_rating}</p>
-            </li>
-          ))}
-        </ul>
+        {isExpanded && (
+          <>
+            <ul>
+              <li>
+                <p>{experience}</p>
+              </li>
+            </ul>
+            <ul>
+              {reviews.map((review, index) => (
+                <li key={index}>
+                  <img src={review.review_avatar} alt="avatar" />
+                  <p>Reviewer: {review.reviewer_name}</p>
+                  <p>Rating: {review.reviewer_rating}</p>
+                  <p>{review.comment}</p>
+                </li>
+              ))}
+                      </ul>
+                      <button type='button'>Book trial lesson</button>
+          </>
+        )}
+    
+        <button onClick={toggleExpand} type='button'>
+          {isExpanded ? 'Show less' : 'Read more'}
+        </button>
         <ul>
           {levels.map((level, index) => (
             <li key={index}>{level}</li>
