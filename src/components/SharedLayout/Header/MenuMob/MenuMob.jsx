7faa1dcbf4425a57733c75666c94fe9def017eaf
Krislv4nk <kristinalev4enko89@gmail.com>
2024-07-20 22:59:03 +0200

    fixed menu mob

--- src/components/SharedLayout/Header/MenuMob/MenuMob.jsx
+++ src/components/SharedLayout/Header/MenuMob/MenuMob.jsx
@@ -10,22 +10,21 @@ import css from './MenuMob.module.css';
 import sprite from '../../../../assets/sprite.svg';
 import { ThemeProvider } from '../ThemeProvider/ThemeProvider';
 
-export const MenuMob = ({onClose}) => {
-const [openLoginModal, setOpenLoginModal] = useState(false);
+export const MenuMob = ({ onClose }) => {
+  const [openLoginModal, setOpenLoginModal] = useState(false);
   const [openRegisterModal, setOpenRegisterModal] = useState(false);
- const [openLogOutModal, setOpenLogOutModal] = useState(false);
+  const [openLogOutModal, setOpenLogOutModal] = useState(false);
   const [isSignUp, setIsSignUp] = useState(true);
   const [isSignIn, setIsSignIn] = useState(localStorage.getItem('isLogin') === 'true');
 
   const openLogOutModalHandler = () => {
     setOpenLogOutModal(true);
-
   };
 
   const openLoginModalHandler = () => {
     setOpenLoginModal(true);
     setOpenRegisterModal(false);
-     setIsSignUp(false);
+    setIsSignUp(false);
   };
 
   const openRegisterModalHandler = () => {
@@ -39,69 +38,118 @@ const [openLoginModal, setOpenLoginModal] = useState(false);
     setOpenRegisterModal(false);
     setOpenLogOutModal(false);
   };
+
   const handleSwitchToLogin = () => {
     setIsSignUp(false);
     setOpenRegisterModal(false);
     setOpenLoginModal(true);
   };
+
   const handleLogoutSuccess = () => {
     setIsSignIn(false);
+    onClose(); 
+  };
+
+  const handleMenuItemClick = () => {
+    onClose(); 
   };
 
   return (
-        <div className={css.wrapper}>
-        <button className={css.iconClose} onClick={onClose}>
-          <svg  width={20} height={20}>
-            <use xlinkHref={`${sprite}#icon-cross`}></use>
-          </svg>
-        </button>
-        <nav className={css.nav}>
+    <div className={css.wrapper}>
+      <button className={css.iconClose} onClick={onClose}>
+        <svg width={20} height={20}>
+          <use xlinkHref={`${sprite}#icon-cross`}></use>
+        </svg>
+      </button>
+      <nav className={css.nav}>
         <ul className={css.navList}>
-          <ThemeProvider/>
-  <li><Link className={css.link} to="/home">
-         Home
-        </Link></li>
-  <li> <Link className={css.link} to="/teachers">
-          Teachers
-          </Link></li>
-          {isSignIn ?
+          <ThemeProvider />
+          <li>
+            <Link className={css.link} to="/home" onClick={handleMenuItemClick}>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link className={css.link} to="/teachers" onClick={handleMenuItemClick}>
+              Teachers
+            </Link>
+          </li>
+          {isSignIn ? (
             <li>
-              <Link className={css.link} to="/favorites">Favorites</Link>
-            </li> : null}
-</ul>
+              <Link className={css.link} to="/favorites" onClick={handleMenuItemClick}>
+                Favorites
+              </Link>
+            </li>
+          ) : null}
+        </ul>
       </nav>
-        {isSignIn ? <div className={css.authBox}>
-            <button className={css.logInButton} onClick={openLoginModalHandler} type='button'>
-          <svg className={css.iconLogIn} width={20} height={20}>
+      {isSignIn ? (
+        <div className={css.authBox}>
+          <button className={css.logInButton} onClick={openLoginModalHandler} type="button">
+            <svg className={css.iconLogIn} width={20} height={20}>
               <use xlinkHref={`${sprite}#icon-user`}></use>
-        </svg>Your profile</button>
-            <button onClick={openLogOutModalHandler} className={css.logOutButton} type='button'>
-           LogOut
-                </button>
-                <StyledEngineProvider injectFirst>
-        <Dialog open={openLogOutModal} onClose={closeModalHandler} className={css.backdrop}
-           PaperComponent={() => <LogOutModal onClose={closeModalHandler} isSignUp={isSignUp} onLogoutSuccess={handleLogoutSuccess}/>} />
-      </StyledEngineProvider>
-            
-</div> : <ul className={css.authBox}>
-         <li><button className={css.logInButton} onClick={openLoginModalHandler} type='button'>
-          <svg className={css.iconLogIn} width={20} height={20}>
-              <use xlinkHref={`${sprite}#icon-login`}></use>
-        </svg>Log In</button>
+            </svg>
+            Your profile
+          </button>
+          <button onClick={openLogOutModalHandler} className={css.logOutButton} type="button">
+            LogOut
+          </button>
           <StyledEngineProvider injectFirst>
-        <Dialog open={openLoginModal} onClose={closeModalHandler} className={css.backdrop}
-           PaperComponent={() => <AuthForm onClose={closeModalHandler} isSignUp={isSignUp} onSwitchToLogin={handleSwitchToLogin}/>} />
-      </StyledEngineProvider>
-        </li>
-        <li><button onClick={openRegisterModalHandler} className={css.registrButton} type='button'>Registration</button>
-          <StyledEngineProvider injectFirst>
-        <Dialog open={openRegisterModal} onClose={closeModalHandler} className={css.backdrop}
-           PaperComponent={() => <AuthForm onClose={closeModalHandler} isSignUp={isSignUp} onSwitchToLogin={handleSwitchToLogin} />} />
-      </StyledEngineProvider>
-        </li>
-      </ul>}
-      </div>
-   
-    )
- 
+            <Dialog
+              open={openLogOutModal}
+              onClose={closeModalHandler}
+              className={css.backdrop}
+              PaperComponent={() => (
+                <LogOutModal onClose={closeModalHandler} isSignUp={isSignUp} onLogoutSuccess={handleLogoutSuccess} />
+              )}
+            />
+          </StyledEngineProvider>
+        </div>
+      ) : (
+        <ul className={css.authBox}>
+          <li>
+            <button className={css.logInButton} onClick={openLoginModalHandler} type="button">
+              <svg className={css.iconLogIn} width={20} height={20}>
+                <use xlinkHref={`${sprite}#icon-login`}></use>
+              </svg>
+              Log In
+            </button>
+            <StyledEngineProvider injectFirst>
+              <Dialog
+                open={openLoginModal}
+                onClose={closeModalHandler}
+                className={css.backdrop}
+                PaperComponent={() => (
+                  <AuthForm
+                    onClose={closeModalHandler}
+                    isSignUp={isSignUp}
+                    onSwitchToLogin={handleSwitchToLogin}
+                  />
+                )}
+              />
+            </StyledEngineProvider>
+          </li>
+          <li>
+            <button onClick={openRegisterModalHandler} className={css.registrButton} type="button">
+              Registration
+            </button>
+            <StyledEngineProvider injectFirst>
+              <Dialog
+                open={openRegisterModal}
+                onClose={closeModalHandler}
+                className={css.backdrop}
+                PaperComponent={() => (
+                  <AuthForm
+                    onClose={closeModalHandler}
+                    isSignUp={isSignUp}
+                    onSwitchToLogin={handleSwitchToLogin}
+                  />
+                )}
+              />
+            </StyledEngineProvider>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
 };
