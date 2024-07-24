7987f3283293b0c7a76a174aa74148b8f7158d06
Krislv4nk <kristinalev4enko89@gmail.com>
2024-07-24 18:09:37 +0200

    added lazy

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { Route, Routes, Navigate } from 'react-router-dom';
+import { Suspense, lazy } from "react";
+import { Loader } from "components/Loader/Loader.jsx";
 import PrivateRoute from './components/Auth/Routes/PrivateRoute';
 import SharedLayout from 'components/SharedLayout/SharedLayout';
-import HomePage from 'pages/HomePage/HomePage';
-import Teachers from 'pages/Teachers/Teachers';
-import Favorites from 'pages/Favorites/Favorites';
+
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage.jsx"));
+const Teachers = lazy(() => import("./pages/Teachers/Teachers.jsx"));
+const Favorites = lazy(() => import("./pages/Favorites/Favorites.jsx"));
+
+
 
 function App() {
  
   return (
-    
+    <Suspense fallback={<Loader />}>
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
@@ -19,7 +25,7 @@ function App() {
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    
+    </Suspense>
   );
 }
 export default App;
